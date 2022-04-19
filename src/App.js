da0956df81abb25ad92e8bdec997edcbcbf2f7ec
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-04-19 23:40:35 +0530

    hero icons added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
 import Header from './components/Header';
-
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"
+import Hero from './Hero';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header/>
+            <Hero/>
+          </Route>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+   <div>
+       <div>
+           <h1>This is place to talk</h1>
+       </div>
+   </div>
+    </div>
+  )
+}
+
+export default Hero
